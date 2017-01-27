12acaec6ee8011e6c8324f71134bf59a8cead301
amratsingh <amratkota@gmail.com>
2017-01-28 00:53:04 +0530

    Create Alarm Manager.java

---
+++ AlarmManager/Alarm Manager.java
@@ -0,0 +1,40 @@
+package com.developer.alarm;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this.getApplicationContext(), 1, intent, 0);
+
+                AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pendingIntent);
+            }
+        });
+    }
+}
