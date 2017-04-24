366ff695f49df341f83e0e9a90d1d17eb4b4fa03
amratsingh <amratkota@gmail.com>
2017-04-24 18:14:23 +0530

    Update and rename Firebase Notification/BetRatesFirebaseIDService.java to 7. Firebase Notification/MyFirebaseIDService.java

--- 7. Firebase Notification/MyFirebaseIDService.java
+++ 7. Firebase Notification/MyFirebaseIDService.java
@@ -1,11 +1,11 @@
-package com.betrates.service;
+package com.service;
 
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-public class BetRatesFirebaseIDService extends FirebaseInstanceIdService {
+public class MyFirebaseIDService extends FirebaseInstanceIdService {
     @Override
     public void onTokenRefresh() {
         super.onTokenRefresh();
