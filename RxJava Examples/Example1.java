6d49713824261d66da6c1ab2f5a117929a58b8cc
amratsingh <amratkota@gmail.com>
2017-04-24 17:59:39 +0530

    Add files via upload

---
+++ RxJava Examples/Example1.java
@@ -0,0 +1,33 @@
+package com.rx.example;
+
+//    Action Immediately with 3 methods
+
+public class Example1 {
+
+//        myObservable = Observable.just("Hello");
+
+
+//        myObserver = new Observer<String>() {
+//            @Override
+//            public void onCompleted() {
+//                // Called when the observable has no more data to emit
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                // Called when the observable encounters an error
+//            }
+//
+//            @Override
+//            public void onNext(String s) {
+//
+//                Toast.makeText(MainActivity.this, "Received: " + s, Toast.LENGTH_SHORT).show();
+//
+////                Log.d("MY OBSERVER", s);
+//                Log.d("Current Thread", Thread.currentThread().getName());
+//            }
+//        };
+
+
+//            Subscription mySubscription = myObservable.subscribe(myObserver);
+}
